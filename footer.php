0499068f31f56bd71e1240d35bcbd1ab833c6590
NANA Herve <herve_nana@yahoo.fr>
2020-04-04 20:34:51 -0300

    modification du footer

--- footer.php
+++ footer.php
@@ -2,16 +2,17 @@
 
 	<footer id="main-footer">
 		<center>
-	
-		<span class="muted">Université de Guyane -- CHAMOT &copy;
-			<?php setlocale(LC_TIME, "C"); 
-		echo strftime('%Y'); ?>
-			
-		</span>
-	</center>
+
+			<span class="muted">Université de Guyane -- CHAMOT &copy;
+				<?php setlocale(LC_TIME, "C");
+				echo strftime('%Y'); ?>
+
+			</span>
+		</center>
 
 
-</footer>
+	</footer>
 </div>
 </body>
+
 </html>
